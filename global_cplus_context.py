92967a0654aa550c5000b4eb4f77a77c470f8b31
hbroichh <heike.broichhausen@ansys.com>
2023-03-28 15:04:19 +0100

    add to queries

--- global_cplus_context.py
+++ global_cplus_context.py
@@ -36,37 +36,51 @@ sample_code = """
 """
 
 prompt_query1 = """"
-    Please review the sample_code for logical mistakes and don't add comments on the context_code
+    Please review the sample_code for logical mistakes and don't add any comments for the context_code
     """
 
 prompt_query2 = """"
     Please review the sample_code for readability and logical mistakes based on the context_code provided and don't write the context_code
     and give suggestions for the sample_code only
     """
-    
-prompt = prompt_query1
-
-prompt += "context_code:"
 file_list = ['ParentClass.h','ParentClass.cpp','ChildClass.h','ChildClass.cpp']
 
-for file in file_list:
-    with open(file, 'r') as file:
-        data = file.read().replace('\n', '')
-    prompt += data
+def generate_prompt(prompt_query: str, file_list: list) -> str:
+    prompt = prompt_query
+    
+    prompt += "context_code:"
+    for file in file_list:
+        with open(file, 'r') as file:
+            data = file.read().replace('\n', '')
+        prompt += data
+    
+    prompt += "some basic rules to check:"
+    prompt += "comments"
+    prompt += "public in front of constructor"
+    
+    return prompt
 
-prompt += "some basic rules to check:"
-prompt += "comments"
-prompt += "public in front of constructor"
+def call_openai(prompt: str, sample_code: str):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a very kind reviewer."},
+            {"role": "user", "content": prompt + sample_code},
+            # {"role": "assistant", "content": init_response},
+            # {"role": "user", "content": elab}
+        ]
+    )
 
-response = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[
-        {"role": "system", "content": "You are a very kind reviewer."},
-        {"role": "user", "content": prompt + sample_code},
-        # {"role": "assistant", "content": init_response},
-        # {"role": "user", "content": elab}
-    ]
-)
+    text = response['choices'][0].message.content
+    print(text)
 
-text = response['choices'][0].message.content
-print(text)
+
+print("----------------------------LOGICAL ISSUES: ----------------------------------------")
+prompt = generate_prompt(prompt_query1, file_list)
+call_openai(prompt,sample_code)
+
+print("----------------------------READABILITY ENHANCEMENT: ----------------------------------------")
+prompt = generate_prompt(prompt_query2, file_list)
+call_openai(prompt,sample_code)
+
+
