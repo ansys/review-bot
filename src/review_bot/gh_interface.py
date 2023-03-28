e1de1973db6aaa9d1be92a055b3cdba1ccd03dce
Alex Kaszynski <akascap@gmail.com>
2023-03-28 15:47:14 +0200

    Add GH Interface (#6)

---
+++ src/review_bot/gh_interface.py
@@ -0,0 +1,113 @@
+import logging
+import os
+import threading
+
+import requests
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel("DEBUG")
+
+
+def _fetch_file_content(file_data, headers):
+    """Fetch the content of a single file."""
+    content_response = requests.get(
+        file_data["contents_url"], headers=headers, timeout=10
+    )
+
+    if content_response.status_code == 200:
+        content = content_response.json()
+        file_data["file_text"] = requests.get(content["download_url"], timeout=10).text
+    else:
+        raise RuntimeError("Error fetching file content")
+
+
+def _get_token():
+    """Return the github access token from the GITHUB_TOKEN environment variable."""
+    access_token = os.environ.get("GITHUB_TOKEN")
+    if access_token is None:
+        raise OSError('Missing "GITHUB_TOKEN" environment variable')
+    return access_token
+
+
+def get_changed_files_and_contents(owner, repo, pull_number):
+    """Retrieve the filenames, status, and contents of files changed in a GitHub PR.
+
+    Parameters
+    ----------
+    owner : str
+        The owner of the repository where the pull request was made.
+    repo : str
+        The name of the repository where the pull request was made.
+    pull_number : int
+        The number of the pull request to retrieve the changed files for.
+
+    Returns
+    -------
+    list[dict]
+        A list of dictionaries, where each dictionary represents a file that
+        was changed in the pull request. The dictionary contains the following
+        keys:
+
+        - filename: str, the name of the file
+        - status: str, the status of the file change ('added', 'modified', or 'removed')
+        - file_text: str, the contents of the file as a string
+
+    Raises
+    ------
+    RuntimeError
+        If an error occurs while fetching the file content.
+
+    Notes
+    -----
+    This function uses the GitHub API to retrieve the list of changed files in
+    a pull request, along with the contents of each changed file. It requires a
+    GitHub access token with appropriate permissions to access the repository.
+
+    Example
+    -------
+    >>> files = get_changed_files_and_contents('my-org', 'my-repo', 123)
+    >>> print(files[0]['filename'])
+    'path/to/my-file.py'
+    >>> print(files[0]['status'])
+    'modified'
+    >>> print(files[0]['file_text'])
+    'print("Hello, world!")\n'
+
+    """
+    access_token = _get_token()
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}/files"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = requests.get(url, headers=headers, timeout=10)
+
+    if response.status_code != 200:
+        raise RuntimeError(f"Error fetching pull request files: {response.status_code}")
+
+    files = response.json()
+
+    access_token = _get_token()
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}/files"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = requests.get(url, headers=headers, timeout=10)
+
+    if response.status_code != 200:
+        print(f"Error fetching pull request files: {response.status_code}")
+        return
+
+    files = response.json()
+    threads = []
+
+    for file_data in files:
+        LOG.info("Filename: %s", file_data["filename"])
+        LOG.info("Status: %s", file_data["status"])
+        LOG.info("Content:")
+
+        t = threading.Thread(target=_fetch_file_content, args=(file_data, headers))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return files
