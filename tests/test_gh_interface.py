a4d48b7a24b0f1364d63d4cb7bf5d6580188e629
Alex Kaszynski <akascap@gmail.com>
2023-03-28 06:54:33 -0600

    add gh interface

---
+++ tests/test_gh_interface.py
@@ -0,0 +1,35 @@
+"""Test GitHub interface.
+
+90% of this was written using chat gpt with the following prompt:
+
+> Make this a pytest compatible test, and make it just a function test, do not make it a class.
+
+"""
+
+import pytest
+
+from review_bot.gh_interface import get_changed_files_and_contents
+
+# Should be this repository
+OWNER = "ansys"
+REPO = "hackathon-review-bot"
+
+
+def test_get_changed_files_and_contents():
+    pull_number = 4
+
+    files = get_changed_files_and_contents(OWNER, REPO, pull_number)
+
+    assert isinstance(files, list)
+    assert len(files) > 0
+    assert isinstance(files[0], dict)
+    assert "filename" in files[0]
+    assert "status" in files[0]
+    assert "file_text" in files[0]
+
+
+def test_get_changed_files_and_contents_raises_runtime_error():
+    pull_number = 999999  # this is an issue
+
+    with pytest.raises(RuntimeError):
+        get_changed_files_and_contents(OWNER, REPO, pull_number)
