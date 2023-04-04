3e46a6ae7e3c9b62f4c0bc27ed6efeb93668bbca
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-04-04 13:30:31 +0200

    FEAT: exposing access to GH token input

--- tests/test_gh_interface.py
+++ tests/test_gh_interface.py
@@ -5,6 +5,7 @@
 > Make this a pytest compatible test, and make it just a function test, do not make it a class.
 
 """
+import os
 
 import pytest
 
@@ -19,6 +20,20 @@ def test_get_changed_files_and_contents():
     pull_number = 4
 
     files = get_changed_files_and_contents(OWNER, REPO, pull_number)
+
+    assert isinstance(files, list)
+    assert len(files) > 0
+    assert isinstance(files[0], dict)
+    assert "filename" in files[0]
+    assert "status" in files[0]
+    assert "file_text" in files[0]
+
+
+def test_get_changed_files_and_contents_with_input_token():
+    pull_number = 4
+
+    access_token = os.environ.get("GITHUB_TOKEN")
+    files = get_changed_files_and_contents(OWNER, REPO, pull_number, access_token)
 
     assert isinstance(files, list)
     assert len(files) > 0
